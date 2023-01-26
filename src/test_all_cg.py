7f162d1203c4e9080ca17d4166d6101c1cae6f93
osmanakar <osmanakar1123@hotmail.com>
2023-01-25 23:15:23 -0800

    test_all_cg cleanup

--- src/test_all_cg.py
+++ src/test_all_cg.py
@@ -1,8 +1,6 @@
 # This is an example test code for the paper 
-# The test code solves the linear system A x = b, where A is pressure matrix, and b is the velocity divergence
-# Both A and b is creted from a simulation. We provide various simulations, such as smoke plume, rotating fluid, etc, 
-# for reader to pick to test.
-# A and b also depends on the frame number
+# The test code solves the linear system Ax = b, where A is the system matrix, and b is the velocity divergence
+# Both A and b is creted from a simulation. We provide various simulations, such as smoke plume, smoke passing bunny 
 
 #%% Load the required libraries
 import sys
@@ -26,19 +24,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-N", "--resolution", type=int, choices=[64, 128, 256],
                     help="N or resolution of test", default = 64 )
 parser.add_argument("-k", "--trained_model_type", type=int, choices=[64, 128],
-                    help="which model to test", default=128)
+                    help="which model to test", default=64)
 parser.add_argument("-f", "--float_type", type=int, choices=[16, 32],
                     help="model parameters' float type", default=32)
 parser.add_argument("-ex", "--example_type", type=str, choices=["smoke_plume", "smoke_passing_bunny"],
                     help="example type", default="smoke_passing_bunny")
 parser.add_argument("-fn", "--frame_number", type=int,
-                    help="example type", default=10)
+                    help="which frame in sim to test", default=10)
 parser.add_argument("--max_cg_iter", type=int,
                     help="maximum cg iteration", default=1000)
 parser.add_argument("-tol","--tolerance", type=float,
-                    help="tolerance for both DGCM and CG algorithm", default=1.0e-4)
-parser.add_argument("--verbose_dgcm", type=bool,
-                    help="prints residuals of DGCM algorithm for each iteration", default=False)
+                    help="tolerance for both DCDM and CG algorithm", default=1.0e-4)
+parser.add_argument("--verbose_dcdm", type=bool,
+                    help="prints residuals of DCDM algorithm for each iteration", default=False)
 parser.add_argument("--dataset_dir", type=str, required=True,
                     help="path to the dataset")
 parser.add_argument("--CUDA_VISIBLE_DEVICES", type=str,
@@ -48,7 +46,7 @@ parser.add_argument("--num_vectors_deflated_pcg", type=int,
 parser.add_argument('--skip_dcdm', action="store_true", 
                     help='skips dcdm tests')
 parser.add_argument('--skip_icpcg', action="store_true", 
-                    help='skips icpcg/ldlt test')
+                    help='skips icpcg test')
 parser.add_argument('--skip_deflated_pcg', action="store_true", 
                     help='skips deflated pcg test')
 parser.add_argument('--skip_cg', action="store_true", 
@@ -80,37 +78,22 @@ max_cg_iter = args.max_cg_iter
 
 tol = args.tolerance 
 
-verbose_dgcm = args.verbose_dgcm
+verbose_dcdm = args.verbose_dcdm
 
-verbose_icpcg = verbose_dgcm
+verbose_icpcg = verbose_dcdm
 
 dataset_path = args.dataset_dir
 
-#this determines if DCDM uses only CPU or GPU. By default it only uses CPU.
+# This determines if DCDM uses only CPU or uses GPU. By default it only uses CPU.
 os.environ["CUDA_VISIBLE_DEVICES"]= args.CUDA_VISIBLE_DEVICES
 
 #%% 
-# if matrix does not change in the example, use the matrix for the first frame.  
 if example_name in ["smoke_plume", "smoke_passing_bunny"]:
     matrix_frame_number = 1
 else:
     matrix_frame_number = frame_number
     
-#%% Setup The Dimension and Load the Model
-#Decide which dimention to test for:  64, 128, 256, 384, 512 (ToDo)
-#N = 128 # parser 1
-#Decide which model to run: 64 or 128 and float type F16 (float 16) or F32 (float32)
-# There are two types of models: k=64 and k=128, where the models trained over 
-# the matrices ...
-# k defines which parameters and model to be used. Currently we present two model.
-# k = 64 uses model trained model
-#dataset_path = "/data/"
-# change this to where you put the dataset folder
 trained_model_name = dataset_path + "/trained_models/model_N"+str(N)+"_from"+str(k)+"_F"+str(float_type)+"/"
-
-#%% Load the matrix, vectors, and solver
-
-#Decide which example to run for: SmokePlume, ...
 
 #%% Getting RHS for the Testing
 d_type='double'
@@ -129,36 +112,23 @@ A_file_name = dataset_path + "/test_matrices_and_vectors/N"+str(N)+"/"+example_n
 A = hf.readA_sparse(N, A_file_name,'f')
 b = get_vector_from_source(b_file_name)
 CG = cg.ConjugateGradientSparse(A)
-
-#%%
-# parameters for CG
 normalize_ = False 
-
 
 #%% Testing
 if not args.skip_dcdm:
-    #% Setup The Dimension and Load the Model
-    #Decide which dimention to test for:  64, 128, 256, 384, 512 (ToDo)
-    #Decide which model to run: 64 or 128 and float type F16 (float 16) or F32 (float32)
-    # There are two types of models: k=64 and k=128, where the models trained over 
-    # 64^3 and 128^3 computational grids
-    # k defines which parameters and model to be used. Currently we present two model.
-    # k = 64 uses model trained model
-    #"/data/dataset_mlpcg" # change this to where you put the dataset folder
     print("Loading the model...")
     trained_model_name = dataset_path + "/trained_models/model_N"+str(N)+"_from"+str(k)+"_F"+str(float_type)+"/"
     model = hf.load_model_from_source(trained_model_name)
     model.summary()
     print("Model loaded. Number of parameters in the model is ",model.count_params())
-    # This is the lambda function that is needed in DGCM algorithm
-    model_predict = lambda r: model(tf.convert_to_tensor(r.reshape([1,N,N,N]),dtype=dtype_),training=False).numpy()[0,:,:].reshape([N**3]) #first_residual
-    #Dummy Calling
+    model_predict = lambda r: model(tf.convert_to_tensor(r.reshape([1,N,N,N]),dtype=dtype_),training=False).numpy()[0,:,:].reshape([N**3]) 
+    # Dummy Calling
     model_predict(b)
     
     print("DCDM is running...")
     t0=time.time()                                  
     max_dcdm_iter = 100                                                                                                                                      
-    x_sol, res_arr= CG.dcdm(b, np.zeros(b.shape), model_predict, max_dcdm_iter, tol, False ,verbose_dgcm)
+    x_sol, res_arr= CG.dcdm(b, np.zeros(b.shape), model_predict, max_dcdm_iter, tol, False ,verbose_dcdm)
     time_cg_ml = time.time() - t0
     print("DCDM took ", time_cg_ml," secs.")
 
@@ -177,7 +147,6 @@ if not args.skip_deflated_pcg:
     print("Deflated PCG took ",time_cg, " secs")
 
 if not args.skip_icpcg:
-    #Load L matrix, where A ~= L*L^T. L is precomputed 
     b2 = b[A.getnnz(1)>0]
     A2 = A[A.getnnz(1)>0]
     A2 = A2[:,A.getnnz(0)>0]
